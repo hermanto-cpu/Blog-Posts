7f38a219c2ae60149d3352dee6a710be310336fa
Hermanto Prastyawan <hermanto9pras@gmail.com>
2023-01-08 20:12:56 +0700

    melanjutkan registrasion

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -4,22 +4,29 @@
 <div class="row justify-content-center">
     <div class="col-lg-5">
         <main class="form-registration">
+          
             <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>
-            <form>
+            <form action="/register" method="post">
+            @csrf
             <div class="form-floating">
-                <input type="text" name="name" class="form-control" id="name" placeholder="Nama">
+                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Nama">
                 <h6><label for="name">Nama lengkap</label></h6>
+                @error ('name')
+                <div class="invalid-feedback">
+                  <h6>{{ $message }}</h6>
+                </div>
+                @enderror
                 </div>  
             <div class="form-floating">
-                <input type="text" name="username" class="form-control" id="username" placeholder="Username">
+                <input type="text" name="username" class="form-control @error('name') is-invalid @enderror" id="username" placeholder="Username">
                 <h6><label for="name">Username</label></h6>
                 </div>  
               <div class="form-floating">
-                <input type="email" class="form-control" name="email" id="email" placeholder="Email adress">
+                <input type="email" class="form-control @error('name') is-invalid @enderror" name="email" id="email" placeholder="Email adress">
                 <h6><label for="email">Email address</label></h6>
               </div>
               <div class="form-floating">
-                <input type="password" class="form-control" id="password" placeholder="Password">
+                <input type="password" class="form-control @error('name') is-invalid @enderror" id="password" placeholder="Password">
                 <h6><label for="password">Password</label></h6>
               </div>
 
